ddbff95ee777082a2fb90ae914d3f788481ebbff
sampath kumar <sampath.surineni@gmail.com>
2013-03-03 13:44:37 +0530

    Added class

--- Alarm/src/Alarm_Clock.java
+++ Alarm/src/Alarm_Clock.java
@@ -1,11 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Alarm_Clock {
 
 	/**
 	 * @param args
 	 */
+	private static final JFrame backFrame=new JFrame("Alarm Clock");
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		backFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel backPane = new JPanel();
+    	JPanel timeHold = new JPanel();
+    	JPanel bottom = new JPanel();
+    	JPanel vBottom = new JPanel();
+		
 
 	}
 
