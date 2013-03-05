1346dccf80b34ba9488e2ec4fbecaf7f1635c9d8
sampath kumar <sampath.surineni@gmail.com>
2013-03-05 15:10:57 +0530

    2 c

--- Alarm/src/Alarm_Clock.java
+++ Alarm/src/Alarm_Clock.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +8,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
---
+++ src/Alarm.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vinnu
+ */
+public class Alarm {
+    private static String alarmTime = "-1";
+    public Alarm()
+	{
+		//sets default alarm time of -1
+		alarmTime = "-1";
+	}
+    
+}
