2e620a926e54ab2118db47777e4b7fc54f0c315b
vinnu <vinnu@vinnu-HP-Mini-110-3500>
2013-03-07 10:44:36 +0530

    import done

--- src/alarm/Alarm_Clock.java
+++ src/alarm/Alarm_Clock.java
@@ -4,9 +4,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -18,12 +20,12 @@ import javax.swing.JPanel;
 
 public class Alarm_Clock {
 
-	/**
-	 * @param args
-	 */
-	private static final JFrame backFrame=new JFrame("Alarm Clock");
-	protected static final JLabel time = new JLabel("00:00:00");
-	protected static JButton set = new JButton("Set Alarm");
+	private static Alarm anAlarm;
+    private static boolean military = true;
+    private static String fileLocation = "default_alarms" + File.separator + "newalarm1.wav";
+    private static final JFrame backFrame = new JFrame("Alarm Clock");
+    protected static final JLabel time = new JLabel("00:00:00");
+    protected static JButton set = new JButton("Set Alarm");
     private static JButton snooze = new JButton("Snooze");
 	public static void main(String[] args) {
 		backFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,6 +64,77 @@ public class Alarm_Clock {
 			"you can mail the developer at"
                                         +"sampath.surineni@gmail.com");}
 		});
+                
+                 fileMenuItem = new JMenuItem("Cancel Alarm");
+        fileMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                backFrame.setTitle("Alarm Clock");
+                anAlarm = new Alarm();
+            }
+        });
+        fileMenu.add(fileMenuItem);
+        JMenuItem toggleFormat = new JMenuItem("Toggle 'Military time'");
+        toggleFormat.setMnemonic('T');
+        toggleFormat.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                military = !military;
+            }
+        });
+        fileMenuItem = new JMenuItem("Set Alarm Ringer");
+        fileMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //find "ringers" .wav files to use
+                File randomFile;
+                final JFileChooser chooser = new JFileChooser();
+                try {
+                    //create a file to get current directory and navigate to the default_alarms page
+                    randomFile = new File(new File(".").getCanonicalPath() + File.separator + "default_alarms");
+                    //here File it is taking string path name
+                    chooser.setCurrentDirectory(randomFile);
+                    chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                } catch (Exception ex) {
+                    //catch if you can't make the file or something went wrong
+                    System.err.println("Could not create file at current path");
+                    ex.printStackTrace();
+                }
+                int number = chooser.showOpenDialog(null);
+                if (number == JFileChooser.APPROVE_OPTION) {
+                    //if you select a file
+                    File loadMe = chooser.getSelectedFile();
+                    //set fileLocation to that file's location if it is a valid format
+                    if (loadMe.getName().contains(".wav") || loadMe.getName().contains(".mp3")) {
+                        fileLocation = loadMe.getPath();
+                    } else {
+                        //display a dialog saying that we can't open that file if it is not a .wav
+                        JOptionPane.showMessageDialog(backFrame, "Please select a .wav or .mp3 file to set as your alarm.");
+                    }
+                    //System.out.println(loadMe.getPath());
+                }
+            }
+        });
+
+        fileMenu.add(fileMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(toggleFormat);
+        fileMenu.addSeparator();
+        fileMenuItem = new JMenuItem("Exit Program");
+        fileMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //if you click Exit program... the program exits
+                System.exit(0);
+            }
+        });
+        backPane.add(timeHold);
+        fileMenu.add(fileMenuItem);
+        bottom.add(set);
+        bottom.add(off);
+        vBottom.add(snooze);
+        backPane.add(timeHold);
+        backPane.add(bottom);
+        backPane.add(vBottom);
+        backFrame.add(backPane);
+        //add the JMenuBar so you can use it
+        backFrame.setJMenuBar(menuBar);
 	
 
 	}
